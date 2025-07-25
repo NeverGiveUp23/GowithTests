3912da6ba216fef9f3f7e64d42689205e9a4caab
felix <felixvjr0@gmail.com>
2025-07-25 14:22:57 -0400

    creating maps with test and dictionary type with helper assert function

--- maps/maps.go
+++ maps/maps.go
@@ -1,11 +1,24 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
 type Search interface {
 	Dictionary() string
 }
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -2,13 +2,35 @@ package maps
 
 import "testing"
 
-func TestSearch(t *testing.T) {
+func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	dictionary.Add(word, definition)
 
-	assertStrings(t, got, want)
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+		if got == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertError(t, got, ErrNotFound)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -16,4 +38,22 @@ func assertStrings(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find word:", err)
+	}
+	assertStrings(t, got, definition)
 }
